e6a3fae29c405f565fe94636548258e7c9dab2b1
Marcel Richter <richterthepok@yahoo.de>
2019-08-11 15:34:19 +0200

    Fix Questions, Added Soundsystem, cleaned up Quiz

---
+++ BrainBoostBreak.Server/Migrations/20190811124233_FixTypo.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrainBoostBreak.Server.Migrations
+{
+    public partial class FixTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 1,
+                column: "Text",
+                value: "Wem wird folgendes Zitat zugeschrieben: 'Wer glaubt, etwas zu sein, hat aufgehört, etwas zu werden.'");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 2,
+                column: "Text",
+                value: "Wem wird folgendes Zitat zugeschrieben: 'Gewalt ist die Waffe des Schwachen.'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 1,
+                column: "Text",
+                value: "Wer wird folgendes Zitat zugeschrieben: 'Wer glaubt, etwas zu sein, hat aufgehört, etwas zu werden.'");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 2,
+                column: "Text",
+                value: "Wer wird folgendes Zitat zugeschrieben: 'Gewalt ist die Waffe des Schwachen.'");
+        }
+    }
+}
